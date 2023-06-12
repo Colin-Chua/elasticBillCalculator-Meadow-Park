01c971ad572e40b37c7906ee3e3ef7ad5553d61f
Colin-Chua <chuacolin1@gmail.com>
2023-06-13 01:25:25 +0800

    updated

--- pages/bill-summary/index.tsx
+++ pages/bill-summary/index.tsx
@@ -1,22 +1,46 @@
-import { useRouter } from 'next/router';
-import { CalculateBill } from '../bill-calculator/calculate';
-import { RoomBillType } from '@/components/utils/types';
+import { useRouter } from "next/router";
+import { CalculateBill } from "../bill-calculator/calculate";
+import { Bill, RoomBillType, UserRoomBill } from "@/components/utils/types";
+import { Card, Table, TableColumnsType } from "antd";
 
 const BillSummary: React.FC = () => {
   const router = useRouter();
   const params = router.query.data;
-
-  if (!params) {
-    return <div>Loading...</div>;
-  }
-
   const data = JSON.parse(decodeURIComponent(params as string));
 
-  console.log(data);
   const result = CalculateBill(data as any);
 
-  console.log(result)
-  return <div></div>;
+  const columns: TableColumnsType<UserRoomBill> = [
+    {
+      title: "Room",
+      dataIndex: "roomID",
+      width: 200,
+    },
+    {
+      title: "Power Usage",
+      dataIndex: "usage",
+      render: (_, val) => <div>{val.usage.toFixed(2)}</div>,
+    },
+    {
+      title: "Bill",
+      dataIndex: "roomBill",
+      render: (_, val) => <div>RM{val.roomBill.toFixed(2)}</div>,
+    },
+    {
+      title: "People",
+      dataIndex: "people",
+    },
+    {
+      title: "Bill for each 1 people",
+      render: (_, val) => <div>RM{(val.roomBill / val.people).toFixed(2)}</div>,
+    },
+  ];
+
+  return (
+    <div>
+      <Table columns={columns} dataSource={result.result.rooms} />
+    </div>
+  );
 };
 
 export default BillSummary;
